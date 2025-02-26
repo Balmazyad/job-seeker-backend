c57cf047b1f3dccfb1b638f453a749948f9bcd24
Bassam Almazyad <bassam.almazyad@gmail.com>
2025-02-26 14:59:55 +1100

    Added /api/data endpoint

--- index.js
+++ index.js
@@ -6,10 +6,19 @@ const app = express();
 // Use CORS Middleware
 app.use(cors());
 
+// Use JSON Middleware to handle POST requests
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
+// Root endpoint
 app.get('/', (req, res) => {
     res.send('Your endpoint is working!');
+});
+
+// New API endpoint
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'This is sample data from the API' });
 });
 
 app.listen(port, () => {
